4afe6681d76df6a01306fc18dce7b8ea0d29634b
Jay Patel <60284892+imjp19@users.noreply.github.com>
2021-12-05 13:31:57 +0530

    theme added

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import {
+    BaseStyles,
+    Badge,
+    Box,
+    Button,
+    Card,
+    Checkbox,
+    Container,
+    Flex,
+    Grid,
+    Heading,
+    Input,
+    Label,
+    Link,
+    NavLink,
+    Radio,
+    Select,
+    Slider,
+    Text,
+    Textarea
+  } from 'theme-ui'
+  import Head from 'next/head'
+  import Meta from '@hackclub/meta'
+  import theme from '@hackclub/theme'
+  import ColorSwitcher from '../components/color-switcher'
+  import { TypeScale, ColorPalette } from '@theme-ui/style-guide'
+  
+  const DocsPage = () => (
+    <>
+      <Head>
+        <title>Hack Club SVIT</title>
+        <Meta
+          name="Hack Club SVIT"
+          description="Hack Club SVIT Website"
+        />
+      </Head>
+      <Box as="header" sx={{ bg: 'sheet', color: 'text' }}>
+        <Container sx={{ pt: 5, pb: [3, 4], textAlign: 'center' }}>
+          <ColorSwitcher />
+          <Heading as="h1" variant="title" color="red">
+            Hack Club SVIT
+          </Heading>
+          <Text as="p" variant="subtitle" mt={3}>
+            Stay tuned...
+          </Text>
+         </Container>
+      
+      </Box>
+     
+    </>
+  )
+  
+  export default DocsPage
