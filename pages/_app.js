1aca98b44c5cfeb8b7f8e98616acac5ae065d58e
Jay Patel <60284892+imjp19@users.noreply.github.com>
2021-12-05 14:33:38 +0530

    resolverd

--- pages/_app.js
+++ pages/_app.js
@@ -8,30 +8,6 @@ import theme from '@hackclub/theme'
 import { ThemeProvider } from 'theme-ui'
 import ColorSwitcher from '../components/color-switcher'
 
-export default ({
-  name = 'Hack Club SVIT ',
-  title = 'Hack Club SVIT',
-  description = '',
-  image = '',
-  url = ''
-}) => (
-  <Head>
-    <title>{title}</title>
-    <meta property="og:title" content={title} />
-    <meta name="twitter:title" content={title} />
-    <meta name="og:url" content={url} />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content={name} />
-    <meta name="description" content={description} />
-    <meta property="og:description" content={description} />
-    <meta name="twitter:description" content={description} />
-    <meta property="og:image" content={image} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={image} />
-    <meta name="msapplication-TileColor" content={theme.colors.primary} />
-    <meta name="theme-color" content={theme.colors.primary} />
-  </Head>
-)
 export default class App extends NextApp {
   render() {
     
