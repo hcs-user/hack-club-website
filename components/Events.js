2ee21ee0194d512e155ac125a12ffcd547d78280
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-10 16:52:29 +0530

    Modal Added Without Data

--- components/Events.js
+++ components/Events.js
@@ -5,29 +5,50 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import logo from '../public/logo.png'
 import events from "../events.json"
 import { MdOutlineDateRange } from "react-icons/md"
+import { useState } from 'react';
 export default function Events() {
-    console.log(events);
+    const [isModalOPen, setIsModalOpen] = useState(false);
+    const [id, setId] = useState();
     return (
-        <Container id="event" p={2} mb={6} sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
-            <Box >
-                <Text sx={{ fontSize: "4rem", color: 'primary', fontWeight: 500 }}>
-                    Events
-                </Text>
-                <Grid gap={4} columns={[1, null, 4]}>
-                    {
-                        events && events.map(el => {
-                            return <Card m={2} sx={{ height: "auto", width: "200px" }} >
-                                <Image src={logo} />
-                                <Box marginLeft={["-50", null, "-15px"]} sx={{ textAlign: "left", width: "max-content" }}>
-                                    <Text>{el.event_name}</Text><br />
-                                    <MdOutlineDateRange /><Text sx={{ fontSize: '14px' }}>{el.event_date}</Text>
-                                </Box>
-                            </Card>
-                        })
-                    }
-                </Grid>
+        <>
+            <Box bg={'white'} sx={{
+                position: "fixed",
+                top: (isModalOPen ? "35%" : "-50%"),
+                left: "35%",
+                width: "35%",
+                height: "45%",
+                borderRadius: "5px",
+                zIndex: "1",
+                color: "black",
+                transition: "all 0.2s ease-in-out"
+            }}>
+                <Text onClick={() => setIsModalOpen(false)}>Close</Text><br />
+                <Text>ID is :{id}</Text>
             </Box>
-        </Container>
+            <Container id="event" p={2} mb={6} sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+                <Box >
+                    <Text sx={{ fontSize: "4rem", color: 'primary', fontWeight: 500 }}>
+                        Events
+                    </Text>
+                    <Grid gap={4} columns={[1, 2, 3, 4]}>
+                        {
+                            events && events.map(el => {
+                                return <Card key={el.id} m={2} sx={{ height: "auto", width: "200px" }} onClick={() => { setIsModalOpen(true); setId(el.id) }} >
+                                    <Image src={logo} />
+                                    <Box marginLeft={["-50", null, "-15px"]} sx={{ textAlign: "left", width: "max-content" }}>
+                                        <Text>
+                                            {el.event_name}
+                                        </Text><br />
+                                        <MdOutlineDateRange /><Text sx={{ fontSize: '14px' }}>{el.event_date}</Text>
+                                    </Box>
+                                </Card>
+                            })
+                        }
+                    </Grid>
+                </Box>
+            </Container>
+        </>
     )
+
 
 }
