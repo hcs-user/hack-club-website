311a9051f9b78ab786cbfce54f7838a4aafb5e4c
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-11 19:05:46 +0530

    Design updated

--- components/Events.js
+++ components/Events.js
@@ -1,34 +1,54 @@
 import React from 'react'
-import { Box, Button, Card, Container, Flex, Grid, Link, Text } from 'theme-ui'
+import { Box, Button, Card, Container, Flex, Grid, Heading, Link, Text } from 'theme-ui'
 import Image from 'next/image'
-import { Swiper, SwiperSlide } from 'swiper/react';
 import logo from '../public/logo.png'
 import events from "../events.json"
-import { MdOutlineDateRange } from "react-icons/md"
+import { MdOutlineDateRange, MdClose } from "react-icons/md"
 import { useState } from 'react';
 export default function Events() {
     const [isModalOPen, setIsModalOpen] = useState(false);
     const [id, setId] = useState(0);
     return (
         <>
-            <Box bg={'white'} sx={{
+            <Box bg={'aliceblue'} sx={{
                 position: "fixed",
-                top: (isModalOPen ? "35%" : "-50%"),
-                left: "35%",
-                width: "35%",
-                height: "45%",
+                top: (isModalOPen ? "15%" : "-60%"),
+                left:["5%","35%","25%"],
+                width:["100%","50%"],
+                maxWidth:"90%",
+                height: "auto",
                 borderRadius: "5px",
+                padding: "25px",
                 zIndex: "1",
                 color: "black",
-                transition: "all 0.2s ease-in-out"
+                transition: "all 0.2s ease-in-out",
             }}>
-                <Text onClick={() => setIsModalOpen(false)}>Close</Text><br />
-                <Text>ID is :{id}</Text><br />
-                {events[id].event_name}<br />
-                {events[id].event_date} <br />
-                {events[id].desc}
+                <Heading sx={{
+                    display: "flex",
+                    width: "100%",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    // outline: "2px solid red"
+                    textAlign: "center"
+                }}>
+                    {events[id].event_name}
+                    <div onClick={() => setIsModalOpen(false)}> <MdClose /></div>
+                </Heading><br />
+                <h2> Date:{events[id].event_date}</h2>
+                <br />
+                <h2>
+                    What We have done in {events[id].event_name}
+                </h2>
+                <h3>
+
+                    {events[id].desc}
+                </h3>
             </Box>
-            <Container id="event" p={2} mb={6} sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+            <Container id="event" p={2} mb={6} sx={{
+                textAlign: 'center',
+                display: 'flex',
+                justifyContent: 'center',
+            }}>
                 <Box >
                     <Text sx={{ fontSize: "4rem", color: 'primary', fontWeight: 500 }}>
                         Events
