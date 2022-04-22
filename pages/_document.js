6938624954d250d9161a0b4ad88300af47a24bf3
Vivek Suthar <82870307+VIVEK-SUTHAR@users.noreply.github.com>
2022-04-22 15:56:27 +0530

    Some Random  Meta tags  added for inner peace 😛

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,27 @@ export default class extends Document {
       <Html lang="en">
         <Head>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.min.js"></script>
+          <meta
+            name='description'
+            content='Hack Club is an initiative for students by students. Hack Club is a place that lets you tinker, break, and rebuild. After all, you can not make an omelet without breaking a few eggs '
+          />
+          <meta
+            property='og:title'
+            content="Hack Club SVIT"
+          />
+          <meta property='og:image' content='/logo.png' />
+
+          <meta
+            property='og:description'
+            content='hack Club is an initiative for students by students. Hack Club is a place that lets you tinker, break, and rebuild. After all, you can not make an omelet without breaking a few eggs '
+          />
+          <meta
+            property='og:site_name'
+            content='hackclubsvit.co'
+            key='ogsitename'
+          />
+          <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+          <link rel='shortcut icon' href='/logo.png' type='image/x-icon' />
         </Head>
         <body>
           <InitializeColorMode />
