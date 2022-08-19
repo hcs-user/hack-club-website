3d626d91516833d2d0c4ccfb2f10457350c6618d
VIVEK <sutharvivek18@gmail.com>
2022-08-19 19:38:55 +0530

    style :about updated

--- components/About.js
+++ components/About.js
@@ -5,60 +5,24 @@ function About() {
   return (
     <Container
       id="about"
-      p={4}
       bg={'#1D1D1D'}
-      display={'flex'}
-      Flex={'wrap'}
       sx={{
         textAlign: 'center',
         minWidth: '100%',
-        // width: '100vh',
-        minHeight: '80vh',
-        paddingTop: '140px'
+        minHeight: "fit-content",
+        paddingBlock:[48,null,64],
+        display: "flex",
+        flexDirection: ["column", "row"],
+        alignItems: "center",
+        justifyContent: "space-evenly"
       }}
     >
-      <Box
-        px={40}
-        sx={{
-          paddingLeft: '35px',
-          borderRadius: '40px',
-          float: 'left'
-        }}
-      >
-        <img src={'./2.jpg'} width={'300px'} height={'300px'} />
+      <Box sx={{ height: "auto", width: [250, null, 300], flex: 0.2 }}>
+        <img src='/2.jpg' height={"100%"} width={"100%"} className="rounded"></img>
       </Box>
-      <Text
-        variant="title"
-        // paddingLeft={540}
-        // p={4}
-        sx={{
-          fontSize: ['2rem', '2.6rem'],
-          color: 'primary',
-          marginTop: '600px'
-        }}
-      >
-        Who are we?
-      </Text>
-
-      <Box
-        sx={{
-          minWidth: '90%',
-          marginTop: '40px',
-          textAlign: 'center',
-          color: '#E9E9E9'
-        }}
-      >
-        <Text
-          sx={{
-            fontSize: ['1.3rem'],
-            lineHeight: '1'
-          }}
-        >
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum optio
-          explicabo voluptatibus quae consequatur enim, dolore officiis nulla
-          distinctio adipisci, cumque rerum odit dignissimos odio a, corporis
-          obcaecati facere i
-        </Text>
+      <Box sx={{ flex: 0.5,display:'flex',flexDirection:"column" }} >
+        <Text color='primary' sx={{ fontSize: 52 }}>Who are we</Text>
+        <Text sx={{ fontSize: 24 }}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum optio explicabo voluptatibus quae consequatur enim, dolore officiis nulla distinctio adipisci, cumque rerum odit dignissimos odio a, corporis obcaecati facere i</Text>
       </Box>
     </Container>
   )
